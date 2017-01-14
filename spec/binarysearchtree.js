8159f88e64c365cf415523d337a20f44c90df79b
Keith Oppel <koppel@Keiths-MacBook-Pro.local>
2017-01-13 18:20:13 -0800

    implemented Binary Search Tree (everything except"remove" and "traverse")

--- spec/binarysearchtree.js
+++ spec/binarysearchtree.js
@@ -4,25 +4,24 @@ import BinarySearchTree from '../src/binarysearchtree'
 
 chai.use(chaiChange)
 
-const generateTestList = () => {
-  const list = new BinarySearchTree()
+const generateTestTree = () => {
+  const bst = new BinarySearchTree()
 
-  list.insert(1)
-  list.insert(2)
-  list.insert(3)
-  list.insert(4)
-  list.insert(5)
+  bst.insert(6)
+  bst.insert(3)
+  bst.insert(8)
+  bst.insert(7)
 
-  return list
+  return bst
 }
-describe('BinarySearchTree', () => {
+describe.only('BinarySearchTree', () => {
   'use strict'
 
   it('exists', () => {
     expect(BinarySearchTree).to.be.a('function')
   })
 
-  describe.only('insert()', () => {
+  describe('insert()', () => {
 
     it('increments the count when inserting a node', () => {
       const aBinarySearchTree = new BinarySearchTree()
@@ -32,12 +31,8 @@ describe('BinarySearchTree', () => {
     })
 
     it('inserts a node with the specified value into the tree.', () => {
-      const aBinarySearchTree = new BinarySearchTree()
+      const aBinarySearchTree = generateTestTree()
 
-      aBinarySearchTree.insert(6)
-      aBinarySearchTree.insert(3)
-      aBinarySearchTree.insert(8)
-      aBinarySearchTree.insert(7)
 
       expect(aBinarySearchTree.root.data)
         .to.equal(6)
@@ -48,40 +43,82 @@ describe('BinarySearchTree', () => {
       expect(aBinarySearchTree.root.right.left.data)
         .to.equal(7)
     })
+
+    it('sets parent node flags appropriately', () => {
+      const aBinarySearchTree = generateTestTree()
+
+      expect(aBinarySearchTree.root.parent)
+        .to.equal(null)
+      expect(aBinarySearchTree.search(3).parent.data)
+        .to.equal(6)
+    })
   })
 
   describe('search()', () => {
-    it('returns a node object or null if not found.', () => {
-      const aBinarySearchTree = generateTestList()
+    it('returns null if the tree is empty.', () => {
+      const aBinarySearchTree = new BinarySearchTree()
 
-      expect(() => aBinarySearchTree.insert('junk'))
-        .to.alter(() => aBinarySearchTree.size(), { from: 3, to: 4 })
+      expect(aBinarySearchTree.search('nope'))
+        .to.equal(null)
+    })
+
+    it('returns a matching root node', () => {
+      const aBinarySearchTree = generateTestTree()
+
+      expect(aBinarySearchTree.search(6))
+        .to.equal(aBinarySearchTree.root)
+    })
+
+    it('returns a matching branch/leaf node', () => {
+      const aBinarySearchTree = generateTestTree()
+
+      expect(aBinarySearchTree.search(3))
+        .to.equal(aBinarySearchTree.root.left)
     })
   })
 
   describe('remove()', () => {
     it('removes a node with a given value from the tree if the value exists.', () => {
-      const aBinarySearchTree = new BinarySearchTree()
+      const aBinarySearchTree = generateTestTree()
 
-      expect(aBinarySearchTree.isEmpty())
-        .to.equal(true)
+      expect(() => aBinarySearchTree.remove(7))
+        .to.alter(() => aBinarySearchTree.count(), { from: 4, to: 3 })
+      expect(aBinarySearchTree.root.right.left)
+        .to.equal(null)
+    })
+
+    it('returns false if the node w/ given data is not found', () => {
+      const aBinarySearchTree = generateTestTree()
+
+      expect(aBinarySearchTree.remove('nope'))
+        .to.alter(() => aBinarySearchTree.count(), { from: 4, to: 4 })
     })
   })
 
   describe('traverse()', () => {
     it('traverse the tree in the defined order (either preOrder, inOrder, or postOrder) and apply function on each node value', () => {
-      const aBinarySearchTree = new BinarySearchTree()
+      const aBinarySearchTree = generateTestTree()
 
-      expect(aBinarySearchTree.isEmpty())
-        .to.equal(true)
+      const orderValues = (order) => {
+        const result = []
+        aBinarySearchTree.traverse(order, (x) => result.push(x))
+        return result
+      }
+
+      expect(orderValues(aBinarySearchTree, 'inOrder'))
+        .to.equal([3, 6, 7, 8])
+      expect(orderValues(aBinarySearchTree, 'preOrder'))
+        .to.equal([6, 3, 8, 7])
+      expect(orderValues(aBinarySearchTree, 'postOrder'))
+        .to.equal([3, 7, 8, 6])
     })
   })
 
   describe('count()', () => {
     it('return the number of nodes in the tree', () => {
-      const aBinarySearchTree = generateTestList()
-      expect(aBinarySearchTree.head.data)
-        .to.equal('first')
+      const aBinarySearchTree = generateTestTree()
+      expect(aBinarySearchTree.count())
+        .to.equal(4)
     })
   })
 })
--- src/binarysearchtree.js
+++ src/binarysearchtree.js
@@ -1,10 +1,34 @@
 'use strict'
 
 class Node {
-  constructor( data ) {
+  constructor( data, parent=null ) {
+    this.parent = parent
     this.left = null
     this.right = null
     this.data = data
+  }
+
+  find (direction) {
+    let side, node
+
+    switch (direction) {
+      case 'left':
+      [side, node] = ['left', this.left]
+      break
+
+      case 'right':
+      [side, node] = ['right', this.right]
+      break
+
+      case 'parent':
+      let position = this.parent.left.value === this.value
+       ? 'left'
+       : 'right'
+      [side, node] = [position, this.parent.find(position)]
+      break
+    }
+
+     return [side, node]
   }
 }
 
@@ -16,44 +40,76 @@ export default class BinarySearchTree {
   }
   // inserts a node with the specified value into the tree.
   insert ( data ) {
+    const tree = this
     const node = new Node(data)
     this.size++
 
-    const findTargetNode = ( data, currentNode ) => {
+    const findTargetNode = function( data, currentNode ) {
       if (!currentNode) {
-        this.root = node
+        tree.root = node
         return
       }
 
-      let targetNode = data < currentNode.data
-       ? currentNode.left
-       : currentNode.right
+      let [direction, targetNode] = data < currentNode.data
+       ? currentNode.find('left')
+       : currentNode.find('right')
 
-      if (!targetNode) {
-        data < currentNode.data
-         ? (() => currentNode.left = node)()
-         : (() => currentNode.right = node)()
-         return
-      }
+      return targetNode
+       ? findTargetNode( data, targetNode )
+       : (() => {
+         node.parent = currentNode
 
-      return findTargetNode( data, targetNode )
+         direction === 'left'
+          ? (() => currentNode.left = node)()
+          : (() => currentNode.right = node)()
+       })()
     }
 
-    findTargetNode( data, this.root )
+    findTargetNode( data, tree.root )
   }
   // returns a node object or null if not found.
   search ( data ) {
+    const findTargetNode = function( data, currentNode ) {
+      return !currentNode ? currentNode : (() => {
+        if (currentNode.data === data) { return currentNode }
 
+        let [direction, targetNode] = data < currentNode.data
+        ? currentNode.find('left')
+        : currentNode.find('right')
+
+        return targetNode.data === data
+        ? targetNode
+        : findTargetNode( data, targetNode )
+      })()
+    }
+
+    return findTargetNode( data, this.root )
   }
 
   // removes a node value (if exists) from the tree.
   remove ( data ) {
-
+    const node = this.search(data)
+    if (node) {
+      node.find(parent) = null
+    }
   }
   // traverse the tree in the defined order (either 'preOrder', 'inOrder', or 'postOrder') and apply function on each node's value.
   // 'inOrder', (val) => console.log(val)
   traverse ( order, callback ) {
+    // InOrder: Until all nodes are traversed −
+    // Step 1 − Recursively traverse left subtree.
+    // Step 2 − Visit root node.
+    // Step 3 − Recursively traverse right subtree.
 
+    // PreOrder: Until all nodes are traversed −
+    // Step 1 − Visit root node.
+    // Step 2 − Recursively traverse left subtree.
+    // Step 3 − Recursively traverse right subtree.
+
+    // PostOrder: Until all nodes are traversed −
+    // Step 1 − Recursively traverse left subtree.
+    // Step 2 − Recursively traverse right subtree.
+    // Step 3 − Visit root node.
   }
   // return the number of nodes in the tree.
   count() {
